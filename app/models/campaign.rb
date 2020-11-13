748add6a8604891cba59a908b325dba341a764e3
Daniel Evani <danielevani97@gmail.com>
2020-11-13 18:14:27 -0300

    Model Campaign, User and Member seteds

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,20 @@
+# frozen_string_literal: true
+
 class Campaign < ApplicationRecord
   belongs_to :user
+  has_many :members, dependent: :destroy
+  before_create :set_member
+  before_create :set_status
+  enum status: %i[pending finished]
+  validates :title, :description, :user, :status, presence: true
+
+  # Inicia a campanha com o status pendente
+  def set_status
+    self.status = :pending
+  end
+
+  # Adiciona o criador da campanha como membro dela
+  def set_member
+    members << Member.create(name: user.name, email: user.email)
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,25 @@
+# frozen_string_literal: true
+
 class Member < ApplicationRecord
   belongs_to :campaign
+  after_save :set_campaign_pending
+  validates :name, :email, :campaign, presence: true
+
+  def set_pixel
+    self.open = false
+    self.token = loop do
+      # Gera um token aleatório
+      # Verifica se outro membro não recebeu o mesmo token
+      random_token = SecureRandom.urlsafe_base64(nil, false)
+      break random_token unless Member.exists?(token: random_token)
+    end
+    save!
+  end
+
+  protected
+
+  # Seta a campanha como pendente, após criar um novo membro
+  def set_campaign_pending
+    campaign.update(status: :pending)
+  end
 end
