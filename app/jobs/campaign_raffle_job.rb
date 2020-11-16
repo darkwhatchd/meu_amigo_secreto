b81f6b577c0481b5833aa3d5168e08c50216fbb7
Daniel Evani <danielevani97@gmail.com>
2020-11-15 21:30:09 -0300

    set campaign raffle job

---
+++ app/jobs/campaign_raffle_job.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CampaignRaffleJob < ApplicationJob
+  queue_as :emails
+
+  def perform(campaign)
+    results = RaffleService.new(campaign).call
+
+    campaign.members.each(&:set_pixel)
+    results.each do |r|
+      CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
+    end
+    campaign.update(status: :finished)
+
+    # if results == false
+    # Send mail to owner of campaign (desafio)
+    # end
+  end
+end
